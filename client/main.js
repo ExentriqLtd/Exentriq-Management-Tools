a98539653c9ef3f28e260f7866f074c923f624e1
Kirill Dubinin <kdubinin@accusoft.com>
2015-12-11 19:28:24 +0300

    Resize circle view on win resize

--- client/main.js
+++ client/main.js
@@ -1,7 +1,26 @@
-if (Meteor.isClient){
-	Template.treeView.onChange(function(){
+if (Meteor.isClient) {
+	
+	var _win = $(window);
+	var root = $('.organization-manager');
+
+	Template.treeView.onChange(function() {
 
 		var m = Template.treeView.getModel();
 		Template.circleView.setModel(m);
+	});
+
+	_win.resize(function() {
+
+		if (_win.width() < 600) {
+			root.addClass('small');
+		} else {
+			root.removeClass('small');
+			model = Template.treeView.getModel();
+			Template.circleView.setModel(model);
+		}
+	});
+
+	$(_win).ready(function(){
+		//_win.trigger('resize');	
 	});
 }
