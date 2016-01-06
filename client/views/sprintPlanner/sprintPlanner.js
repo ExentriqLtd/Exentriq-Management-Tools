4084652f0c692fcbb7cc714e8d2a29c4cf61dda3
Kirill Dubinin <kdubinin@accusoft.com>
2016-01-06 17:43:45 +0300

    Revert SprintPlanner

--- client/views/sprintPlanner/sprintPlanner.js
+++ client/views/sprintPlanner/sprintPlanner.js
@@ -28,22 +28,6 @@ Template.sprintPlanner.onCreated(function () {
 					var query = Spaces.find({
 						cmpId: cmpId
 					});
-
-					Template.treeView.organizationManagerModel.children = convertNode(query.fetch());
-					_interval = setInterval(function() {
-
-						if (Template.treeView.renderDone) {
-							setModel();
-							bindObserveChanges(query);
-
-							var orderingQuery = Ordering.find({
-								cmpId: cmpId
-							});
-							ordering = orderingQuery.fetch()[0];
-							bindObserveChanges(orderingQuery);
-						}
-					}, 10);
-
 					Template.sideMenu.organizationManagerModel.children = convertNode(query.fetch());
 					_interval = setInterval(function() {
 
@@ -69,7 +53,7 @@ Template.sprintPlanner.onCreated(function () {
 	function setModel() {
 
 		clearInterval(_interval);
-		Template.treeView.setModel(Template.sideMenu.organizationManagerModel);
+		Template.sideMenu.setModel(Template.sideMenu.organizationManagerModel);
 	}
 	function convertNode(allSpaces, _param) {
 
