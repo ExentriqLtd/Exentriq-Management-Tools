7b11adb140a8b25c00e6f59872c682a06edba061
Calogero Crapanzano <calosan@gmail.com>
2016-02-08 20:45:41 +0100

    format logged time in activity tracker table, re-add seconds field

--- client/templatesHelper.js
+++ client/templatesHelper.js
@@ -23,6 +23,31 @@ Template.registerHelper('formatLoggedTime', function(logged) {
 	return result;
 });
 
+Template.registerHelper('formatSeconds', function(seconds) {
+
+	var secondsInAMinute = 60;
+    var secondsInAnHour  = 60 * secondsInAMinute;
+    var secondsInADay    = 8 * secondsInAnHour;
+
+    // extract days
+    var days = Math.floor(seconds / secondsInADay);
+
+    // extract hours
+    var hourSeconds = seconds % secondsInADay;
+    var hours = Math.floor(hourSeconds / secondsInAnHour);
+
+    // extract minutes
+    var minuteSeconds = hourSeconds % secondsInAnHour;
+    var minutes = Math.floor(minuteSeconds / secondsInAMinute);
+    
+    var result = '';
+    result += days>0 ? (days + 'd') : '';
+	result += hours>0 ? (' ' + hours + 'h') : '';
+	result += minutes>0 ? (' ' + minutes + 'm') : '';
+
+	return result;
+});
+
 Template.registerHelper('formatLink', function(text) {
 	if(typeof text == 'string'){
 		if(startsWith(text, 'http')){
